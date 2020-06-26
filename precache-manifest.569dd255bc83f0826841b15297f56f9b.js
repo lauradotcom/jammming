a56f560c7b12e4d8b1f5acfb789d9fcf2553765d
Laura <lrhwkns@gmail.com>
2020-06-25 17:09:52 -0700

    Updates

---
+++ precache-manifest.569dd255bc83f0826841b15297f56f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35be3093a00a24a29a4fee157774bfe9",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "cf45eabb6aa699d8802c",
+    "url": "/jammming/static/css/main.e7b4c6eb.chunk.css"
+  },
+  {
+    "revision": "66aae2ff3de0e14a6fc7",
+    "url": "/jammming/static/js/2.c06e17bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.c06e17bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf45eabb6aa699d8802c",
+    "url": "/jammming/static/js/main.cda8f195.chunk.js"
+  },
+  {
+    "revision": "0dfdbff139f3591273ce",
+    "url": "/jammming/static/js/runtime-main.256f6ab3.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
